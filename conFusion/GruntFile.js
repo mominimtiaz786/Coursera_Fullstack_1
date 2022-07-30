b6e042408801ce03adf458ac7c8925a7c83d4b01
mominimtiaz786 <21100179@lums.edu.pk>
2022-07-30 11:41:30 +0500

    Grunt Part 1

---
+++ conFusion/GruntFile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default',['browserSync','watch'])
+
+};
+// npm install -g grunt-cli@1.2.0
+// npm install grunt@1.0.2 --save-dev
+// npm install grunt-sass@2.1.0 time-grunt@1.4.0 jit-grunt@0.10.0 --save-dev
+// npm install grunt-contrib-watch@1.0.0 grunt-browser-sync@2.2.0 --save-dev
